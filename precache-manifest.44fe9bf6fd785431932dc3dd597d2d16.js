fa963afac8c5781333630faffa9943dc6d394262
rifan <rifanripan@gmail.com>
2020-04-15 15:28:41 +0700

    Updates

---
+++ precache-manifest.44fe9bf6fd785431932dc3dd597d2d16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f100501602390ca25a71f1b7acdb471",
+    "url": "/code/index.html"
+  },
+  {
+    "revision": "641fb8618e3fc2b84a7a",
+    "url": "/code/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a488168449eaf296ef1f",
+    "url": "/code/static/css/main.7438ec53.chunk.css"
+  },
+  {
+    "revision": "641fb8618e3fc2b84a7a",
+    "url": "/code/static/js/2.095fa480.chunk.js"
+  },
+  {
+    "revision": "eb56b19eba63a53416f4fd778023529f",
+    "url": "/code/static/js/2.095fa480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a488168449eaf296ef1f",
+    "url": "/code/static/js/main.f77ac835.chunk.js"
+  },
+  {
+    "revision": "30d72231af6ab3b55d42",
+    "url": "/code/static/js/runtime-main.d78a0187.js"
+  }
+]);
